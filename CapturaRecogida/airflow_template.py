44a5766a0375772f9e1b4966345fdb0fd5ddc64c
Roberto Lopez <role@gft.com>
2019-10-01 15:38:32 +0200

    Adding airflow template exercise

--- CapturaRecogida/airflow_template.py
+++ CapturaRecogida/airflow_template.py
@@ -1,63 +1,67 @@
-import datetime
-
-from airflow import DAG
-from airflow.operators.bash_operator import BashOperator
-from airflow.operators.python_operator import PythonOperator
+from datetime import datetime
+from datetime import timedelta
+import airflow
 import requests
 import json
 import pysftp
 import  time;
 
+from airflow import DAG
+from airflow.operators.bash_operator import BashOperator
+from airflow.operators.python_operator import PythonOperator
 
-def download_json_comments(json_file_arg):
 
-	req = requests.get('https://jsonplaceholder.typicode.com/comments')
+default_args = {
+
+    'owner': 'Roberto', #Put your name
+    'retries': 1,
+	'start_date':  datetime.now(),
+    'retry_delay': timedelta(minutes=5),
+}
+
+def download_json_comments():
+
+	#1. Get comments from jsonplaceholder.typicode.com using a REST Call
+	
 	print(req.json())
 
-	with open('comments.json', 'w') as json_file:
-		json.dump(req.json(), json_file_arg)
+	with open("comments.json", 'w') as json_file:
+		json.dump(req.json(), json_file)		
+		
+	print("Successfully writted on comments.json")
+	
 
 
-def upload_sftp(json_file_arg):
+def upload_sftp():
 
 	cnopts = pysftp.CnOpts()
 	cnopts.hostkeys = None
     
-	srv = pysftp.Connection(host="35.222.158.208", username="edemExercise3",
-	password="edemExercise3",cnopts = cnopts)
-
-	with srv.cd('Upload/Roberto'): #chdir to your folder
-		srv.put(json_file_arg) #upload file to it
+	#2. Create a pysftp connection with host="35.222.158.208", username="edemExercise3", password="edemExercise3",cnopts = cnopts
+	
+	json_remote_file = "comments."+str(time.time())+".json"
+	
+	#3. Do a sftp with following parameters
+	# Local file (comments.json)
+	# Remote file path "Upload/YourName/json_remote_file"
 
 	# Closes the connection
 	srv.close()
 
 
-default_args = {
 
-    'owner': 'user', #Put your name
 
-    'retries': 1,
+
+with DAG('airflow_sftp', default_args=default_args, schedule_interval='*/3 * * * *',) as dag:
+
 	
-	'start_date': datetime.datetime(2019, 9, 1),
-
-    'retry_delay': datetime.timedelta(minutes=5),
-
-}
-
-
-
-with DAG('airflow_example', default_args=default_args, schedule_interval='*/3 * * * *',) as dag:
-
-	json_file = "comments."+str(time.time())+".json"
-	
-	download_json_task = PythonOperator(task_id='Get_comments', python_callable=download_json_comments,json_file_arg=json_file)
+	download_json_task = PythonOperator(task_id='Get_comments', python_callable=download_json_comments)
 	
 	print_json_ok = BashOperator(task_id='Download_check', bash_command='echo "Json comments file downloaded successfully"')
 
-	upload_json_sftp_task = PythonOperator(task_id='Upload_comments_sftp', python_callable=upload_sftp,json_file_arg=json_file)
-
+	upload_json_sftp_task = PythonOperator(task_id='Upload_comments_sftp', python_callable=upload_sftp)
+	
 	print_upload_ok = BashOperator(task_id='Upload_check', bash_command='echo "Json comments file uploaded successfully"')
 
 
-download_json_task >> print_json_ok >> upload_json_sftp_task >> print_upload_ok
+#4. Create worflow DAG with following sequence: download_json_task, print_json_ok, upload_json_sftp_task, print_upload_ok
