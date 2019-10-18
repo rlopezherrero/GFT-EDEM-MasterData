c9c051ea3f5e0b5c5c38e35771d9706a519fc4a7
Roberto Lopez <role@gft.com>
2019-10-18 16:12:46 +0200

    Adding a new step

--- CapturaRecogida/airflow_template.py
+++ CapturaRecogida/airflow_template.py
@@ -26,6 +26,7 @@ def download_json_comments():
 	
 	print(req.json())
 
+	#3. Add your name as a Sufix of file name 
 	with open("comments.json", 'w') as json_file:
 		json.dump(req.json(), json_file)		
 		
@@ -38,12 +39,12 @@ def upload_sftp():
 	cnopts = pysftp.CnOpts()
 	cnopts.hostkeys = None
     
-	#3. Create a pysftp connection with host="35.222.158.208", username="edemExercise3", password="edemExercise3",cnopts = cnopts
+	#4. Create a pysftp connection with host="35.222.158.208", username="edemExercise3", password="edemExercise3",cnopts = cnopts
 	
 	json_remote_file = "comments."+str(time.time())+".json"
 	
-	#4. Do a sftp with following parameters
-	# Local file (comments.json)
+	#5. Do a sftp with following parameters
+	# Local file (commentsYourName.json)
 	# Remote file path "Upload/YourName/json_remote_file"
 
 	# Closes the connection
@@ -52,7 +53,7 @@ def upload_sftp():
 
 
 
-#5. Add your name to the DAG name below
+#6. Add your name to the DAG name below
 with DAG('airflow_sftp_YourName', default_args=default_args, schedule_interval='*/3 * * * *',) as dag:
 
 	
@@ -65,4 +66,4 @@ with DAG('airflow_sftp_YourName', default_args=default_args, schedule_interval='
 	print_upload_ok = BashOperator(task_id='Upload_check', bash_command='echo "Json comments file uploaded successfully"')
 
 
-#6. Create worflow DAG with following sequence: download_json_task, print_json_ok, upload_json_sftp_task, print_upload_ok
+#7. Create worflow DAG with following sequence: download_json_task, print_json_ok, upload_json_sftp_task, print_upload_ok
