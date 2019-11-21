9dfee8a3775cff6e3f4bb2c32289d38d3b43f652
Roberto Lopez <rlopezherrero@gmail.com>
2019-11-21 23:22:01 +0100

    Adding Beam Exercise

--- AlmacenamientoProcesamiento/Streaming/BeamExercise/Beam.py
+++ AlmacenamientoProcesamiento/Streaming/BeamExercise/Beam.py
@@ -1,21 +1,3 @@
-#ts
-# Licensed to the Apache Software Foundation (ASF) under one or more
-# contributor license agreements.  See the NOTICE file distributed with
-# this work for additional information regarding copyright ownership.
-# The ASF licenses this file to You under the Apache License, Version 2.0
-# (the "License"); you may not use this file except in compliance with
-# the License.  You may obtain a copy of the License at
-#
-#    http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-
-"""A word-counting workflow."""
 
 from __future__ import absolute_import
 
@@ -69,22 +51,19 @@ def run(argv=None, save_main_session=True):
                       dest='input_topic',
                       #1 Add your project Id and topic name you created
                       # Example projects/versatile-gist-251107/topics/iexCloud',
-                      #default='projects/YOUR_PROJECT_ID/topics/YOUR_TOPIC',
-                      default='projects/versatile-gist-251107/topics/iexCloud',
+                      default='projects/YOUR_PROJECT_ID/topics/YOUR_TOPIC',
                       help='Input file to process.')
   parser.add_argument('--output',
                       dest='output',
                       #2 Add your project Id and Dataset and table name you created
                       # Example versatile-gist-251107:Stocks.quotes',
-                      #default='YOUR_PROJECT_ID:YOUR_DATASET.YOUR_TABLE',
-                      default='versatile-gist-251107:Stocks.quotes',
+                      default='YOUR_PROJECT_ID:YOUR_DATASET.YOUR_TABLE',
                       help='Table where the quotes are stored')
   parser.add_argument('--input_subscription',
                       dest='input_subscription',
                       #3 Add your project Id and Subscription you created you created
                       # Example projects/versatile-gist-251107/subscriptions/quotesConsumer',
-                      #default='projects/YOUR_PROJECT_ID/subscriptions/YOUR_SUBSCRIPION',
-                      default='projects/versatile-gist-251107/subscriptions/quotesConsumer',
+                      default='projects/YOUR_PROJECT_ID/subscriptions/YOUR_SUBSCRIPION',
                       help='Input Subscription')
   
   
@@ -99,16 +78,15 @@ def run(argv=None, save_main_session=True):
   google_cloud_options = pipeline_options.view_as(GoogleCloudOptions)
   #4 Add your project ID
   #Example versatile-gist-251107
-  #google_cloud_options.project = 'YOUR_PROJECT_ID'
-  google_cloud_options.project = 'versatile-gist-251107'
+  google_cloud_options.project = 'YOUR_PROJECT_ID'
   google_cloud_options.job_name = 'myjob'
   #5 Add your created bucket
   #€xample edem-bucket-roberto
-  google_cloud_options.staging_location = 'gs://edem-bucket-roberto/binaries'
-  google_cloud_options.temp_location = 'gs://edem-bucket-roberto/temp'
+  google_cloud_options.staging_location = 'gs://YOUR_BUCKET_ID/binaries'
+  google_cloud_options.temp_location = 'gs://YOUR_BUCKET_ID/temp'
 
-  #pipeline_options.view_as(StandardOptions).runner = 'DirectRunner'
-  pipeline_options.view_as(StandardOptions).runner = 'DataflowRunner'
+  pipeline_options.view_as(StandardOptions).runner = 'DirectRunner'
+  #pipeline_options.view_as(StandardOptions).runner = 'DataflowRunner'
   pipeline_options.view_as(StandardOptions).streaming = True
 
  
