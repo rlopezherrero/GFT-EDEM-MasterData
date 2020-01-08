88f9fe88036b54882c404ec09d131a7d56b5b22d
Edem Student <a10pepo@gmail.com>
2020-01-08 13:12:52 +0100

    added location change

--- Hackathon/Streaming/ElasticWritter.py
+++ Hackathon/Streaming/ElasticWritter.py
@@ -19,6 +19,32 @@ from elasticsearch import Elasticsearch
 import json
 
 
+class LocationConcat(beam.DoFn):
+    """
+    Filter data for inserts
+    """
+
+    def process(self, element):
+        
+        #{"empty_slots":17,"extra":{"address":"Economista Gay - Constituci\xc3\xb3n","banking":false,"bonus":false,"last_update":1578482815000,"slots":20,"status":"OPEN","uid":136},"free_bikes":3,"id":"1f6b81722ca23ce520f77207b868afa9","latitude":39.4899091610835,"longitude":-0.375701108044157,"name":"136_CALLE_ECONOMISTA_GAY","timestamp":"2020-01-08T11:34:20.782000Z"}'
+        
+        item = json.loads(element)
+        return [{'empty_slots':item['empty_slots'],
+                 'free_bikes':item['free_bikes'],
+                 'id':item['id'],
+                 'name':item['name'],
+                 'timestamp':item['timestamp'],
+                 'latitude':item['latitude'],
+                 'longitude':item['longitude'],
+                 'address':item['extra']['address'],
+                 'banking':item['extra']['banking'],
+                 'bonus':item['extra']['bonus'],
+                 'status':item['extra']['status'],
+                 'uid':item['extra']['uid'],
+                 'location':str(item['latitude'])+","+str(item['longitude'])                
+                 }]
+
+
 class IndexDocument(beam.DoFn):
    
     es=Elasticsearch([{'host':'localhost','port':9200}])
@@ -40,14 +66,14 @@ def run(argv=None, save_main_session=True):
                       dest='input_topic',
                       #1 Add your project Id and topic name you created
                       # Example projects/versatile-gist-251107/topics/iexCloud',
-                      default='projects/hackathon-edem/topics/valenbisi',
+                      default='projects/hackaton-264420/topics/valenbisi',
                       help='Input file to process.')
   #2 Replace your hackathon-edem with your project id 
   parser.add_argument('--input_subscription',
                       dest='input_subscription',
                       #3 Add your project Id and Subscription you created you created
                       # Example projects/versatile-gist-251107/subscriptions/quotesConsumer',
-                      default='projects/hackathon-edem/subscriptions/streaming',
+                      default='projects/hackaton-264420/subscriptions/streaming',
                       help='Input Subscription')
   
   
@@ -61,7 +87,7 @@ def run(argv=None, save_main_session=True):
    
   google_cloud_options = pipeline_options.view_as(GoogleCloudOptions)
   #3 Replace your hackathon-edem with your project id 
-  google_cloud_options.project = 'hackathon-edem'
+  google_cloud_options.project = 'hackaton-264420'
   google_cloud_options.job_name = 'myjob'
  
   # Uncomment below and add your bucket if you want to execute on Dataflow
@@ -85,11 +111,17 @@ def run(argv=None, save_main_session=True):
   biciStations = p | beam.io.ReadFromPubSub(subscription=known_args.input_subscription)
 
   # Print messages received
+ 
+  
+  
+  biciStations = ( biciStations | beam.ParDo(LocationConcat()))
+  
   biciStations | 'Print Quote' >> beam.Map(print)
   
   # Store messages on elastic
   biciStations | 'Bici Stations Stored' >> beam.ParDo(IndexDocument())
-
+  
+  
   
  
   result = p.run()
